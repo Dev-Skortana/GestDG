da24510861b51b782f10149917b0a0f37d78e791
Dev-Skortana <abokicontactpro@gmail.com>
2019-11-21 04:01:38 +0100

    Projet v2 création système de recherche page showmembres

--- GestDG/GestDG/ViewsModels/ViewModel_Membre.cs
+++ GestDG/GestDG/ViewsModels/ViewModel_Membre.cs
@@ -1,21 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows.Input;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
+﻿using GestDG.Models;
+using GestDG.Services.Interfaces;
 using Prism.Mvvm;
 using Prism.Navigation;
-using GestDG.Models;
-using GestDG.Services.Interfaces;
-using GestDG.Services.Classes;
-using GestDG.Interface_File_Image_Access;
-using Xamarin.Forms;
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using System.Linq;
 
 namespace GestDG.ViewsModels
 {
-    class ViewModel_Membre : BindableBase,INavigationAware
+    class ViewModel_Membre : BindableBase, INavigationAware
     {
 
         public String title { get; set; } = "Page des membres";
@@ -31,11 +27,21 @@ namespace GestDG.ViewsModels
         public ICommand Cmd
         {
             get {
-                return new Command<String>(async (pseudo)=> {
-                   await load(pseudo);
+                return new Command<String>(async (donnees) => {
+                    await load(new Dictionary<string, string>() { {champ_selected,donnees}},methoderecherche_selected);
                 });
             }
         }
+
+        //private List<String> get_champs_membre() { return Database_Initialize.Database_configuration.Database_Initialize().Result.GetMappingAsync<Membre>().Result.Columns.Cast<String>().ToList(); }
+        public List<String> Liste_champs { get { return new List<string>() { "pseudo", "date_naissance", "age", "date_inscription", "url_site", "url_avatar", "sexe", "localisation", "statut", "rang_nom" }; } }//Database_Initialize.Database_configuration.Database_Initialize().Result.GetMappingAsync<Membre>().Result.Columns; }
+        public String champ_selected { get; set; }
+
+        public List<string> Liste_methodesrecherches { get { return new List<string>() { "Egale a", "Commence par", "Fini par" }; } }
+        public String methoderecherche_selected{ get; set; }
+
+        public List<string> Liste_typesrecherches { get { return new List<string>() { "Simple", "Multiple"}; } }
+        public String type_selected { get; set; }
 
         private List<Membre> _liste_membres;
         public List<Membre> Liste_membres
@@ -50,11 +56,28 @@ namespace GestDG.ViewsModels
             }
         }
 
-        private async Task load(String pseudo="")
+        private Boolean getvalue(Membre membre,Dictionary<String,String> dico,string methode)
+        {
+            Boolean resultat = false;
+            switch (dico.Keys.ToList()[0])
+            {
+                case "pseudo":
+                    resultat = methode.Contains("Commence par") ? membre.pseudo.ToUpper().StartsWith(dico["pseudo"].ToUpper()) : (methode.Contains("Fini par") ? membre.pseudo.ToUpper().EndsWith(dico["pseudo"].ToUpper()) :( methode.Contains("Egale a") ? membre.pseudo.ToUpper() == dico["pseudo"].ToUpper() : membre.pseudo.Contains(dico["pseudo"])));
+                break;
+                case "age":
+                    resultat =(membre.age==Convert.ToInt64(dico["age"]));
+                break;
+                case "sexe":
+                    resultat = (membre.sexe.Contains(dico["sexe"]));
+                break;
+            }
+            return resultat;
+        }
+        private async Task load(Dictionary<String,String> dictionnaire_donnees,String methode_recherche)
         {
            //await service_membre.delete(new Membre());
             //_liste_membres = new List<Membre> { new Membre() { pseudo = "SharQaaL", age = 27, date_naissance = new DateTime(), date_inscription = new DateTime(), url_site = "", url_avatar =await DependencyService.Get<IFilePicture_Access>().save_picture(element_commun.type_image_membre.Avatar) + "/1-72.png", sexe = 'M', localisation = "Amérique", statut = "En ligne", rang = new Rang() { nom_rang = "DT", url_rang = "https://i.servimg.com/u/f56/11/26/28/25/dirige10.png" }, rang_nom = "DT" }, new Membre() { pseudo = "DG dada", age = 14, date_naissance = new DateTime(), date_inscription = new DateTime(), url_site = "", url_avatar = await DependencyService.Get<IFilePicture_Access>().save_picture(element_commun.type_image_membre.Avatar) + "/112-43.jpg", sexe = 'M', localisation = "Amérique", statut = "En ligne", rang = new Rang() { nom_rang = "DT", url_rang = "https://i.servimg.com/u/f56/11/26/28/25/dirige10.png" }, rang_nom = "DT" } };
-            Liste_membres = (List<Membre>)await service_membre.GetList(pseudo); ;//(List<Membre>)await service_membre.GetList("");
+            Liste_membres = (from item in (List<Membre>)await service_membre.GetList(dictionnaire_donnees.ContainsKey("pseudo") ? dictionnaire_donnees["pseudo"]:"") where getvalue(item,dictionnaire_donnees,methode_recherche) orderby item.pseudo select item).ToList();//(List<Membre>)await service_membre.GetList("");
         }
 
         public void OnNavigatedFrom(INavigationParameters parameters)
@@ -64,7 +87,7 @@ namespace GestDG.ViewsModels
 
         public async void OnNavigatedTo(INavigationParameters parameters)
         {
-            await load();
+            await load(new Dictionary<string, string>() { {"pseudo",""} },"");
         }
     }
 }
